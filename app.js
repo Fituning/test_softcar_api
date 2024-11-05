107353e26c78d87db533013d9b1651ece17a704e
Sroÿce <carstar@live.fr>
2024-11-05 11:49:23 +0100

    init

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+
+const userRoutes = require('./routes/user');
+
+
+const bodyParser = require("express");
+const app = express();
+
+mongoose.connect('mongodb+srv://cazancoth:KQ3xZc5Lk2Avbfql@cluster0.cadku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json()); //pour un body en json
+app.use(bodyParser.urlencoded({ extended: true })); // pour un body en x-www-form-urlencoded
+
+
+app.use('/api/auth', userRoutes);
+
+
+module.exports = app;
