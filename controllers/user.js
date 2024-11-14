fca2705178071ae5c02e0fcedff73673e4a02de4
Sroÿce <63264339+Fituning@users.noreply.github.com>
2024-11-14 14:59:08 +0100

    fix user warnings

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-exports.signup = (req, res, next) => {
+exports.signup = (req, res) => {
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
             const user = new User({
@@ -27,7 +27,7 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {
+exports.login = (req, res) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
