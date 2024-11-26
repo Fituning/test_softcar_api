18cfb8eae1a78df487af06bfab37db2fdb434182
Sroÿce <63264339+Fituning@users.noreply.github.com>
2024-11-26 16:18:23 +0100

    add toggle prog activation

--- routes/ac_prog.js
+++ routes/ac_prog.js
@@ -6,6 +6,7 @@ const GetAuthPreferredCar = require('../middleware/get_auth_preferred_car');
 const ACProgCtrl = require('../controllers/prog');
 
 router.post('/',GetAuthPreferredCar, ACProgCtrl.addACProg);
+router.patch('/toggle_activation',GetAuthPreferredCar, ACProgCtrl.toggleProgActivation);
 
 
 module.exports = router;
